a64aa5bdd0788885b7c67a60afb7908c18b7b735
tafelnl <35837839+tafelnl@users.noreply.github.com>
2021-06-22 12:41:36 +0200

    ✨ add Vue.js 3

---
+++ vue3/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="body"></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+#body {
+  height: 100vh;
+  width: 100vw;
+  color: #000;
+}
+</style>
