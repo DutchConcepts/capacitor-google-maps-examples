d5d7d2d6d6717b1612095496cbca937cb8be5c25
tafelnl <35837839+tafelnl@users.noreply.github.com>
2021-06-28 11:00:22 +0200

    ✨  update to `capacitor-googlemaps-native@v2.0.0-alpha.2` and update examples

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="body">
     <div class="col">
-      <div ref="first" style="background: red" @click="startMap('first')">
-        <div class="absolute-square">
-          <span v-if="maps.first">
-            {{ maps.first.currentMarkerId }}
-          </span>
-        </div>
-      </div>
+      <div ref="first" style="background: red" @click="startMap('first')"></div>
+
       <div ref="second" style="background: green" @click="startMap('second')">
-        <div class="absolute-square">
+        <div class="absolute-square" @click.prevent="updateMap('second')">
           <span v-if="maps.second">
             {{ maps.second.currentMarkerId }}
           </span>
         </div>
       </div>
+
       <div ref="third" style="background: blue" @click="startMap('third')">
-        <div class="absolute-square">
+        <div class="absolute-square" @click.prevent="updateMap('third')">
           <span v-if="maps.third">
             {{ maps.third.currentMarkerId }}
           </span>
+        </div>
+        <div
+          class="absolute-square"
+          style="left: auto; right: 50px"
+          @click.prevent="moveCamera('third')"
+        >
+          moveCamera
         </div>
       </div>
     </div>
@@ -29,6 +32,8 @@
 <script>
 import { CapacitorGoogleMaps } from '@capacitor-community/capacitor-googlemaps-native';
 
+import style from '../../common/style.json';
+
 export default {
   name: 'App',
   components: {},
@@ -37,9 +42,59 @@ export default {
       maps: {},
     };
   },
+  computed: {
+    style() {
+      return JSON.stringify(style);
+    },
+  },
   methods: {
     getRandomArbitrary(min, max) {
       return Math.random() * (max - min) + min;
+    },
+    async moveCamera(refName) {
+      const googleMap = this.maps[refName];
+
+      if (!googleMap) {
+        return;
+      }
+
+      const result = await CapacitorGoogleMaps.moveCamera({
+        mapId: googleMap.mapId,
+        cameraPosition: {
+          target: {
+            latitude: -33.86,
+            longitude: 151.2,
+          },
+          zoom: 0,
+        },
+        duration: 1000,
+      });
+
+      alert(JSON.stringify(result));
+    },
+    async updateMap(refName) {
+      const googleMap = this.maps[refName];
+
+      if (!googleMap) {
+        return;
+      }
+
+      const dotShown = confirm('Show dot?');
+
+      const result = await CapacitorGoogleMaps.updateMap({
+        mapId: googleMap.mapId,
+        preferences: {
+          gestures: {
+            isScrollAllowedDuringRotateOrZoom: true,
+          },
+          appearance: {
+            type: refName === 'second' ? 4 : null,
+            isMyLocationDotShown: dotShown,
+          },
+        },
+      });
+
+      alert(JSON.stringify(result));
     },
     async startMap(refName) {
       // if map already (being) initialized, avoid a new map being initialized
@@ -70,31 +125,48 @@ export default {
 
       // finally create the map
       try {
-        const map = await CapacitorGoogleMaps.create({
-          width: Math.round(boundingRect.width),
-          height: Math.round(boundingRect.height),
-          x: Math.round(boundingRect.x),
-          y: Math.round(boundingRect.y),
-          latitude: -33.86,
-          longitude: 151.2,
-          zoom: 12,
+        const result = await CapacitorGoogleMaps.createMap({
+          boundingRect: {
+            width: Math.round(boundingRect.width),
+            height: Math.round(boundingRect.height),
+            x: Math.round(boundingRect.x),
+            y: Math.round(boundingRect.y),
+          },
+          cameraPosition: {
+            target: {
+              latitude: -33.86,
+              longitude: 151.2,
+            },
+            zoom: 12,
+          },
+          preferences: {
+            gestures: {
+              isScrollAllowedDuringRotateOrZoom: false,
+            },
+            appearance: {
+              style: refName === 'first' ? this.style : null,
+              isMyLocationDotShown: true,
+            },
+          },
         });
+
+        alert(JSON.stringify(result));
 
         // remember mapId for curent element for later use
         this.maps[refName] = {
-          mapId: map?.mapId,
+          mapId: result?.googleMap?.mapId,
         };
 
         // set `data-maps-id` attribute for delegating touch events
-        element.setAttribute('data-maps-id', map.mapId);
+        element.setAttribute('data-maps-id', result.googleMap.mapId);
 
         alert('Map loaded successfully');
 
-        if (map?.mapId) {
+        if (result?.googleMap?.mapId) {
           var i;
           for (i = 0; i < 500; i++) {
             CapacitorGoogleMaps.addMarker({
-              mapId: map.mapId,
+              mapId: result.googleMap.mapId,
               latitude: -this.getRandomArbitrary(33, 34),
               longitude: this.getRandomArbitrary(151, 152),
               title: 'Some title',
@@ -105,20 +177,18 @@ export default {
             });
           }
 
-          CapacitorGoogleMaps.didTapMarker(
+          CapacitorGoogleMaps.didTapInfoWindow(
             {
-              mapId: map.mapId,
-              preventDefault: false, // set to true if you do not want the default behaviour
+              mapId: result.googleMap.mapId,
             },
             (result) => {
-              this.maps[refName].currentMarkerId = result.marker.markerId;
-              alert(JSON.stringify(result));
+              alert('didTapInfoWindow: ' + JSON.stringify(result));
             }
           );
 
           CapacitorGoogleMaps.didCloseInfoWindow(
             {
-              mapId: map.mapId,
+              mapId: result.googleMap.mapId,
             },
             (result) => {
               // this will never fire, because it is overwritten by the next `didCloseInfoWindow`
@@ -128,11 +198,60 @@ export default {
 
           CapacitorGoogleMaps.didCloseInfoWindow(
             {
-              mapId: map.mapId,
+              mapId: result.googleMap.mapId,
             },
             (result) => {
               // but this will fire
+              alert('didCloseInfoWindow: ' + JSON.stringify(result));
               this.maps[refName].currentMarkerId = null;
+            }
+          );
+
+          CapacitorGoogleMaps.didTapMap(
+            {
+              mapId: result.googleMap.mapId,
+            },
+            (result) => {
+              alert('didTapMap: ' + JSON.stringify(result));
+            }
+          );
+
+          CapacitorGoogleMaps.didLongPressMap(
+            {
+              mapId: result.googleMap.mapId,
+            },
+            (result) => {
+              alert('didLongPressMap: ' + JSON.stringify(result));
+            }
+          );
+
+          CapacitorGoogleMaps.didTapMarker(
+            {
+              mapId: result.googleMap.mapId,
+              preventDefault: false, // set to true if you do not want the default behaviour
+            },
+            (result) => {
+              this.maps[refName].currentMarkerId = result.marker.markerId;
+              alert('didTapMarker: ' + JSON.stringify(result));
+            }
+          );
+
+          CapacitorGoogleMaps.didTapMyLocationButton(
+            {
+              mapId: result.googleMap.mapId,
+              preventDefault: false, // set to true if you do not want the default behaviour
+            },
+            (result) => {
+              alert('didTapMyLocationButton: ' + JSON.stringify(result));
+            }
+          );
+
+          CapacitorGoogleMaps.didTapMyLocationDot(
+            {
+              mapId: result.googleMap.mapId,
+            },
+            (result) => {
+              alert('didTapMyLocationDot: ' + JSON.stringify(result));
             }
           );
         }
